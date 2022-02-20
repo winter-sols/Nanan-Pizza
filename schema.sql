408308fc4137e88c106668dbe9b610407d83edac
winter-sols <winter.solstice1219@gmail.com>
2022-02-20 11:13:14 +0200

    extras, orderlist and order added to schema

--- schema.sql
+++ schema.sql
@@ -3,3 +3,31 @@ CREATE TABLE users (
     username VARCHAR (10) UNIQUE NOT NULL,
     password VARCHAR (10) NOT NULL
 );
+
+CREATE TABLE pizza (
+    pizza_id SERIAL PRIMARY KEY, 
+    name TEXT UNIQUE NOT NULL, 
+    ingredients TEXT [] NOT NULL, 
+    price NUMERIC (4,2) NOT NULL
+);
+
+CREATE TABLE extras (
+    extra_id SERIAL PRIMARY KEY,
+    name VARCHAR (30) UNIQUE NOT NULL
+    price NUMERIC (4,2) NOT NULL
+);  
+
+CREATE TABLE orderlist (
+    order_id SERIAL PRIMARY KEY, 
+    prod TEXT NOT NULL, 
+    size INTEGER NOT NULL, 
+    extras TEXT []
+);
+
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY, 
+    user VARCHAR (20), 
+    order TEXT, 
+    order_time TIMESTAMP NOT NULL
+);
+
