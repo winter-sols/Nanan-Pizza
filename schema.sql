6f260b2dfec3109abdadaeebeb38e1dd9c533241
winter-sols <winter.solstice1219@gmail.com>
2022-03-06 16:33:26 +0200

    better tables

--- schema.sql
+++ schema.sql
@@ -1,13 +1,15 @@
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR (10) UNIQUE NOT NULL,
-    password VARCHAR (10) NOT NULL
+    password TEXT NOT NULL
+    role INTEGER NOT NULL DEFAULT 2
 );
 
-CREATE TABLE pizza (
-    pizza_id SERIAL PRIMARY KEY, 
+CREATE TABLE products (
+    prod_id SERIAL PRIMARY KEY,
+    category TEXT NOT NULL
     name TEXT UNIQUE NOT NULL, 
-    ingredients TEXT [] NOT NULL, 
+    ingredients TEXT [], 
     price NUMERIC (4,2) NOT NULL
 );
 
@@ -17,11 +19,20 @@ CREATE TABLE extras (
     price NUMERIC (4,2) NOT NULL
 );  
 
+CREATE TABLE sizes (
+    size_id SERIAL PRIMARY KEY,
+    size_name TEXT NOT NULL
+    price NUMERIC (4,2) NOT NULL
+);
+
 CREATE TABLE orderlist (
-    order_id SERIAL PRIMARY KEY, 
-    prod TEXT NOT NULL, 
-    size INTEGER NOT NULL, 
-    extras TEXT []
+    order_id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    product_id INTEGER REFERENCES products,
+    size INTEGER REFERENCES sizes, 
+    extras TEXT [],
+    price NUMERIC (4,2),
+    VISIBLE BOOLEAN
 );
 
 CREATE TABLE orders (
